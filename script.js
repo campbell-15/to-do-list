34b2d205930c97b18fb84bf38755f7e6ebe7fc81
Campbell Davies-Webb <campbelldavieswebb@gmail.com>
2024-02-22 10:52:11 +0200

    Fixed display task

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function addTask() {
     if (taskInput.value.trim() !== "") {
         let listItem = document.createElement("li");
         let checkbox = document.createElement("input");
-        let taskText = document.createTextNode("taskInput.value");
+        let taskText = document.createTextNode(taskInput.value);
         let deleteButton = document.createElement("button");
 
         checkbox.type = "checkbox";
