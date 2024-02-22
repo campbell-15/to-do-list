05a945810def63748bab28a22e8715dc8546618e
Campbell Davies-Webb <campbelldavieswebb@gmail.com>
2024-02-22 10:38:23 +0200

    JavaScript function to add a new task to the list:

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 function addTask() {
-    
+    let taskInput = document.getElementById("taskInput");
+    let taskList = document.getElementById("taskList");
+
+    if (taskInput.value.trim() !== "") {
+        let listItem = document.createElement("li");
+        let checkbox = document.createElement("input");
+        let taskText = document.createTextNode("taskInput.value");
+        let deleteButton = document.createElement("button");
+
+        checkbox.type = "checkbox";
+        deleteButton.textContent = "Delete";
+
+        listItem.appendChild(checkbox);
+        listItem.appendChild(taskText);
+        listItem.appendChild(deleteButton);
+        taskList.appendChild(listItem);
+
+        taskInput.value = "";
+    }
 }
